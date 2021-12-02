6c34629df45c25f1eebc0f6c386fe3768bb567af
baruab <baruab@gmail.com>
2021-12-02 11:44:45 -0500

    Update main.R

--- Final_Project/integration/POC_Recomendation/ui/main.R
+++ Final_Project/integration/POC_Recomendation/ui/main.R
@@ -14,13 +14,20 @@ hidden(fluidRow(
     12,
     div(
       dashboardPage(
-        dashboardHeader(title = "DS Recommender"),
+        dashboardHeader(title = "POC Recommender"),
         dashboardSidebar(
          
           sidebarMenu(
-            menuItem("Dashboard", tabName = "dashboard"),
-            menuItem("Data Extraction", tabName = "dataExtraction"),
-            menuItem("Survey Q&A", tabName = "survey_qa")
+            menuItem("Dashboard", tabName = "dashboard", icon=icon("line-chart"), selected=TRUE),
+            menuItem("Data Extraction", tabName = "dataExtraction", icon=icon("table")),
+            menuItem("Survey Q&A", tabName = "survey_qa", icon=icon("mortar-board")),
+            menuItem("Codes",  icon = icon("file-text-o"),
+                     menuSubItem("global.R", tabName = "global", icon = icon("angle-right")),
+                     menuSubItem("ui.R", tabName = "ui", icon = icon("angle-right")),
+                     menuSubItem("server.R", tabName = "server", icon = icon("angle-right")),
+                     menuSubItem("main.R", tabName = "mainUI", icon = icon("angle-right"))
+            ),
+            menuItem("About", tabName = "about", icon = icon("question"))
           )
         ),
         dashboardBody(
@@ -59,7 +66,59 @@ hidden(fluidRow(
                       )),
                    
                     plotOutput("qaPlot")
+            ),
+            
+            tabItem("dataExtraction",
+                    fluidRow(
+                      column(12,
+                             uiOutput("resumeUrlOutput")   
+                      )),
+                    
+                    plotOutput("resumeKeywordPlot")
+            ),
+            #############################################################
+            tabItem(tabName = "global",
+                    box(width = NULL, status = "primary", solidHeader = TRUE, title= "global.R",
+                        downloadButton('downloadData1', 'Download'),
+                        br(),br(),
+                        pre(includeText("global.R"))
+                        
+                    )
+            ),
+            tabItem(tabName = "ui",
+                    box( width = NULL, status = "primary", solidHeader = TRUE, title="ui.R",
+                         downloadButton('downloadData2', 'Download'),
+                         br(),br(),
+                         pre(includeText("ui.R"))
+                    )
+            ),
+            tabItem(tabName = "server",
+                    box( width = NULL, status = "primary", solidHeader = TRUE, title="server.R",
+                         downloadButton('downloadData3', 'Download'),
+                         br(),br(),
+                         pre(includeText("server.R"))
+                    )
+            ),
+            tabItem(tabName = "mainUI",
+                    box( width = NULL, status = "primary", solidHeader = TRUE, title="main.R",
+                         downloadButton('downloadData4', 'Download'),
+                         br(),br(),
+                         pre(includeText("ui/main.R"))
+                    )
+            ),
+            tabItem(tabName = "about",
+                    fluidPage(
+                      
+                     # HTML("<iframe width=\"960\" height=\"680\" 
+                     #             src=\"https://rpubs.com/bikrambarua/Final_Project_Layout\" 
+                     #             frameborder=\"0\"></iframe>")
+                      tags$iframe(src = "Final_Project_Layout.html",
+                                  width = '100%', height = '800px', 
+                                 frameborder = 0, scrolling = 'auto'
+                       )
+                    )
             )
+            ########################################
           )
         )
       )
